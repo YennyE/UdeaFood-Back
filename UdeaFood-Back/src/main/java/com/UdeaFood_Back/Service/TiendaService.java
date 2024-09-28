6e18b048336953004f515f4d0cd111a3887044e3
YennyE <yenny.tombe@udea.edu.co>
2024-09-28 15:10:23 -0500

    Se agregó código para que la sección se creara por defecto

--- UdeaFood-Back/src/main/java/com/UdeaFood_Back/Service/TiendaService.java
+++ UdeaFood-Back/src/main/java/com/UdeaFood_Back/Service/TiendaService.java
@@ -1,10 +1,14 @@
 package com.UdeaFood_Back.Service;
 
+import com.UdeaFood_Back.Modelo.Seccion;
 import com.UdeaFood_Back.Modelo.Tienda;
+import com.UdeaFood_Back.Repository.ISeccionRepository;
 import com.UdeaFood_Back.Repository.ItiendaRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 @Transactional
@@ -12,11 +16,17 @@ import org.springframework.stereotype.Service;
 public class TiendaService {
 
     private final ItiendaRepository itiendaRepository;
+    private final ISeccionRepository iSeccionRepository;
 
 
 
-    public void crear_tienda (Tienda tienda){
-        itiendaRepository.save(tienda);
+    public void crear_tienda (Tienda tienda){itiendaRepository.save(tienda);
+
+        Seccion seccion = new Seccion();
+        seccion.setIdTienda(tienda.getId());
+        seccion.setNombre("Productos");
+        seccion.setProductos(Collections.emptyList());
+        iSeccionRepository.save(seccion);
     }
 
 }
