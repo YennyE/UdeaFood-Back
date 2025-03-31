fb224778c35ec16950db6b7d093f0f38ba576155
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2025-03-30 19:04:03 -0500

    Agrega Cors para permitir solicitudes desde el front a horarios

--- UdeaFood-Back/src/main/java/com/UdeaFood_Back/Controller/HorarioController.java
+++ UdeaFood-Back/src/main/java/com/UdeaFood_Back/Controller/HorarioController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/horario")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HorarioController {
 
     private final HorarioService horarioService;
