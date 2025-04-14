01af3cf46042583a6ed31f6c01503b78fc58e79d
YeisonCordoba07 <yalexander.cordoba@gmail.com>
2025-04-14 17:47:58 -0500

    Formatea codigo y elimina codigo comentado

--- UdeaFood-Back/src/main/java/com/UdeaFood_Back/Controller/ProductoController.java
+++ UdeaFood-Back/src/main/java/com/UdeaFood_Back/Controller/ProductoController.java
@@ -21,6 +21,7 @@ public class ProductoController {
     private final ProductoService productoService;
 
 
+
     @Operation(summary = "Crear un nuevo producto", description = "Permite crear un nuevo producto")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "El producto se creó exitosamente"),
@@ -34,10 +35,15 @@ public class ProductoController {
         return ResponseEntity.ok("Producto creado");
     }
 
+
+
     @GetMapping("obtener/{id}")
     public ResponseEntity<Producto> obtenerProductoPorId(@PathVariable Integer id) {
         return ResponseEntity.ok(productoService.obtenerProductoPorId(id));
     }
+
+
+
     @GetMapping("/consultar/{nombre}")
     public ResponseEntity<List<Producto>> crearProducto(@PathVariable String nombre) {
         return ResponseEntity.ok(productoService.consultarPorNombre(nombre));
@@ -45,15 +51,15 @@ public class ProductoController {
         //return ResponseEntity.ok("Producto creado");
     }
 
+
+
     @GetMapping("/obtenerTodos/{categoria}")
     public ResponseEntity<List<ProductoDTO>> obtenerTodosLosProductos(@PathVariable String categoria) {
         return ResponseEntity.ok(productoService.obtenerTodosLosProductos(categoria));
     }
-    //para tipo de tieda
-   // @GetMapping("/tipo-tienda/{tipoTienda}")
-    //public List<Producto>obtenerProductosPorTipoTienda(@PathVariable String tipoTienda){
-      //  return productoService.obtenerProductosPorTipoTienda(tipoTienda);
-    //}
+
+
+
     @Operation(summary = "Eliminar un producto", description = "Elimina un producto por su ID")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Producto eliminado exitosamente"),
@@ -69,6 +75,9 @@ public class ProductoController {
             return ResponseEntity.status(404).body(e.getMessage());
         }
     }
+
+
+
     @Operation(summary = "Actualizar un producto", description = "Actualiza los datos de un producto existente")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Producto actualizado exitosamente"),
