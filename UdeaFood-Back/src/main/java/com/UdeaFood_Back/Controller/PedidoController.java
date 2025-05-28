f8297edd42e5db220e45c5022beaac293a407029
YennyE <yenny.tombe@udea.edu.co>
2025-05-27 22:37:17 -0500

    se agrego el endpoint para realizar un pedido

--- UdeaFood-Back/src/main/java/com/UdeaFood_Back/Controller/PedidoController.java
+++ UdeaFood-Back/src/main/java/com/UdeaFood_Back/Controller/PedidoController.java
@@ -1,15 +1,28 @@
 package com.UdeaFood_Back.Controller;
 
+import com.UdeaFood_Back.DTO.PedidoDTO;
+import com.UdeaFood_Back.Modelo.Pedido;
+import com.UdeaFood_Back.Service.PedidoService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/pedido")
 @RequiredArgsConstructor
 public class PedidoController {
+
+    @Autowired
+    PedidoService pedidoService;
+
+    @PostMapping("/crear")
+    public ResponseEntity<Pedido> crearPedido(@RequestBody PedidoDTO pedidoDTO){
+        Pedido pedidoCreado = pedidoService.crearPedido(pedidoDTO);
+        return ResponseEntity.ok(pedidoCreado);
+    }
+
 
 
 
